ec4de8b8bf89a3078dbb66e9e3d3f1f2795df49f
Louay Nasr <LouayNasr@users.noreply.github.com>
2021-06-20 19:52:46 +0300

    implement Notification manager

--- app/src/main/java/com/example/jobschedulerservicebroadcast/MainActivity.java
+++ app/src/main/java/com/example/jobschedulerservicebroadcast/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.jobschedulerservicebroadcast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.queue);
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+
+        OneTimeWorkRequest workRequest = new OneTimeWorkRequest
+                .Builder(MyWorker.class)
+                .setConstraints(constraints)
+                .build();
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WorkManager.getInstance(getApplicationContext()).enqueue(workRequest);
+            }
+        });
     }
 }
